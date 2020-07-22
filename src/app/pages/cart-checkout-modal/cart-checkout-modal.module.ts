4ff4f6b7a9468a667297b33d218f9c9074cb1520
Khwarz <karoumem01@gmail.com>
2020-07-22 14:22:29 +0100

    feat: another super commit for bouka

--- src/app/pages/cart-checkout-modal/cart-checkout-modal.module.ts
+++ src/app/pages/cart-checkout-modal/cart-checkout-modal.module.ts
@@ -1,20 +1,68 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
-import { IonicModule } from '@ionic/angular';
+import { AuthService } from "../auth/auth.service";
 
-import { CartCheckoutModalPageRoutingModule } from './cart-checkout-modal-routing.module';
+import { IonicModule } from "@ionic/angular";
 
-import { CartCheckoutModalPage } from './cart-checkout-modal.page';
+import { CartCheckoutModalPageRoutingModule } from "./cart-checkout-modal-routing.module";
+
+import { CartCheckoutModalPage } from "./cart-checkout-modal.page";
+
+declare var StripeCheckout: StripeCheckoutStatic;
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CartCheckoutModalPageRoutingModule
+    CartCheckoutModalPageRoutingModule,
   ],
-  declarations: [CartCheckoutModalPage]
+  declarations: [CartCheckoutModalPage],
 })
-export class CartCheckoutModalPageModule {}
+export class CartCheckoutModalPageModule {
+  @Input() amount;
+  @Input() description;
+  handler: StripeCheckoutHandler;
+  loading: boolean;
+  confirmation: any;
+
+  constructor(private auth: AuthService) {}
+
+  ngOnInit() {
+    this.handler = StripeCheckout.configure({
+      key: "pk_test_your_key",
+      image: "/your-avatar.png",
+      locale: "auto",
+      source: async (source) => {
+        this.loading = true;
+        const user = await this.auth.getUser();
+        const fun = this.functions.httpsCallable("stripeCreateCharge");
+        this.confirmation = await fun({
+          source: source.id,
+          uid: user.uid,
+          amount: this.amount,
+        }).toPromise();
+        this.loading = false;
+      },
+    });
+  }
+
+  async checkout(e) {
+    const user = await this.auth.getUser();
+    this.handler.open({
+      name: "Fireship Store",
+      description: this.description,
+      amount: this.amount,
+      email: user.email,
+    });
+    e.preventDefault();
+  }
+
+  // Close on navigate
+  @HostListener("window:popstate")
+  onPopstate() {
+    this.handler.close();
+  }
+}
